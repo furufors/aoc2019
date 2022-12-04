4cc6deb4e9be512cf0592b67f13fb528be5998f9
Fredrik Furufors <furufors@gmail.com>
2022-12-04 15:00:55 +0100

    Added day 1

--- day01/day1p1.hs
+++ day01/day1p1.hs
@@ -1,4 +1,4 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . sum . map ((\i -> (i `div` 3) - 2) . (read :: String -> Int)) . lines
--- day01/day1p2.hs
+++ day01/day1p2.hs
@@ -1,4 +1,8 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . sum . map (calculateFuel . (read :: String -> Int)) . lines
+
+calculateFuel :: Int -> Int
+calculateFuel i | i > 0 = let addedFuel = (i `div` 3) - 2 in if addedFuel > 0 then addedFuel + calculateFuel addedFuel else 0
+calculateFuel _ = 0
